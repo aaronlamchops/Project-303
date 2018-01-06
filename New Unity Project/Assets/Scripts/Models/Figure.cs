adfbf9c955a5933705e0823d32131ed5579144b8
Dallin Schwartz <dallinschwartz@Dallins-MacBook-Pro.local>
2018-01-05 21:05:35 -0700

    Added figure.

---
+++ New Unity Project/Assets/Scripts/Models/Figure.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class Figure : MonoBehaviour {
+    public int hp;
+    public int atk;
+
+    private void OnDestroy()
+    {
+        //TODO
+    }
+}
