22ad165b23a14d8d7680ac70ce18adc5320338d1
Aaron Lam <allambchops@gmail.com>
2018-01-06 13:44:16 -0700

    adding new command namespace and basis for command factory

---
+++ Flare/Assets/Scripts/Controllers/Command/CommandFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Command
+{
+    public class CommandFactory
+    {
+        private static CommandFactory _Instance;
+        private static readonly object MyLock = new object();
+        private CommandFactory() { }
+
+        public static CommandFactory Instance
+        {
+            get
+            {
+                lock(MyLock)
+                {
+                    if(_Instance == null)
+                    {
+                        _Instance = new CommandFactory(); 
+                    }
+                }
+                return _Instance;
+            }
+        }
+
+        public Invoker Invoker { get; set; }
+
+        public virtual void CreateAndDo(string commandType, params object[] commandParameters)  //commandParameters is option and can be replace with a delegate
+        {
+            Command command = null;
+
+            switch (commandType.Trim().ToUpper())
+            {
+                case "SKILL":
+                    //command = new AttackCommand(commandParamters);
+                    break;
+            }
+
+            if (command != null)
+            {
+                Invoker.EnqueueCommandForExecution(command);
+            }
+        }
+    }
+}
---
+++ Flare/Assets/Scripts/Controllers/Command/Invoker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Command
+{
+    public class Invoker
+    {
+        private Thread _worker;
+        private bool _keepGoing;
+
+        private readonly Queue<Command> _todoQueue = new Queue<Command>();
+        private readonly AutoResetEvent _enqueueOccurred = new AutoResetEvent(false);
+
+        public void Start()
+        {
+            _keepGoing = true;
+            _worker = new Thread(Run);
+            _worker.Start();
+        }
+
+        public void Stop()
+        {
+            _keepGoing = false;
+        }
+
+        public void EnqueueCommandForExecution(Command cmd)
+        {
+            if (cmd != null)
+            {
+                _todoQueue.Enqueue(cmd);
+                _enqueueOccurred.Set();
+            }
+        }
+
+        private void Run()
+        {
+            while (_keepGoing)
+            {
+                if (_todoQueue.Dequeue().Execute())
+                {
+                    //something when true
+                }
+                else
+                {
+                    _enqueueOccurred.WaitOne(100);
+                }
+            }
+        }
+    }
+}
