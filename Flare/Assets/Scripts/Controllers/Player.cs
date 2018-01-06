7475f62aa47ee7e11a3b054ca566393309538229
Jesse Jensen <jensen.jesse.h@gmail.com>
2018-01-06 01:33:53 -0700

    Added Skills and Player objects

---
+++ Flare/Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Hashtable skills = new Hashtable();
+    private Hashtable inventory = new Hashtable();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void AttemptMove ()
+    {
+
+
+    }
+
+    void Attack()
+    {
+
+
+    }
+
+    void UseSkill()
+    {
+
+
+    }
+
+   void TakeDamage()
+    {
+    }
+
+    void Heal()
+    {
+    }
+}
---
+++ Flare/Assets/Scripts/Models/Skills.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill : MonoBehaviour {
+
+    int enemiesKilled;
+    int level;
+    string skillName;
+
+    Skill(string skill)
+    {
+        enemiesKilled = 0;
+        level = 0;
+        skillName = skill;
+    }
+}
