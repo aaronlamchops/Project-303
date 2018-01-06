4b81a10aa7d2b68b81bcd9ae56a9615c6802c5e8
Aaron Lam <allambchops@gmail.com>
2018-01-05 21:57:53 -0700

    temp commit

--- New Unity Project/Assets/Scripts/SceneManager.cs
+++ New Unity Project/Assets/Scripts/SceneManager.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour {
-
+public class SceneManager : MonoBehaviour 
+{
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+
 	}
+
+	public void LoadScene(string scene)
+	{
+		Application.LoadLevel (scene);
+	}
+
 }
